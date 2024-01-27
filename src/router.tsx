67f835004ce30850ecec94c576d17c8ef92ef016
Gabriel Kogima <gabriel_kogima@hotmail.com>
2024-01-27 15:17:20 -0300

    feat: add react-router-dom and routes setup

---
+++ src/router.tsx
@@ -0,0 +1,16 @@
+import { Home } from "@/pages";
+import { Layout } from "@/components";
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
