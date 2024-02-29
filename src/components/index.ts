7dfd8843fbae40ad8b862798ff73d0976a70adaa
Gabriel Kogima <gabriel_kogima@hotmail.com>
2024-02-29 19:58:45 -0300

    refactor: adds components to folder index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@ export * from "./Layout";
 export * from "./Header";
 export * from "./Button";
 export * from "./InvoiceFilter";
+export * from "./InvoiceList";
+export * from "./InvoiceListItem";
+export * from "./InvoiceStatusTag";
