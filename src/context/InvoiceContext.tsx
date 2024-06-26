56c85609b0f8103f58eca60dfec954a81e585892
Gabriel Kogima <gabriel_kogima@hotmail.com>
2024-06-25 21:32:56 -0300

    refactor: moves invoice reducer to its own file

--- src/context/InvoiceContext.tsx
+++ src/context/InvoiceContext.tsx
@@ -11,12 +11,7 @@ import {
 import { Invoice, InvoiceStatus } from "@/types";
 import { useLocalStorage } from "@/hooks";
 import data from "@/const/data.json";
-
-export type InvoiceAction =
-  | { type: "set"; payload: Invoice[] }
-  | { type: "add"; payload: Invoice }
-  | { type: "update"; payload: Invoice }
-  | { type: "delete"; payload: Invoice };
+import { invoicesReducer, InvoiceAction } from "./invoiceReducer";
 
 type InvoiceStore = {
   invoices: Invoice[];
@@ -30,27 +25,6 @@ type InvoiceStore = {
 };
 
 const InvoiceContext = createContext<InvoiceStore>({} as InvoiceStore);
-
-function invoicesReducer(invoices: Invoice[], { type, payload }: InvoiceAction): Invoice[] {
-  switch (type) {
-    case "set":
-      return payload;
-    case "add":
-      return [...invoices, payload];
-    case "delete":
-      return invoices.filter((invoice) => invoice.id !== payload.id);
-    case "update":
-      return invoices.map((invoice) => {
-        if (invoice.id == payload.id) {
-          return payload;
-        }
-
-        return invoice;
-      });
-    default:
-      return invoices;
-  }
-}
 
 export function useInvoices() {
   return useContext(InvoiceContext);
---
+++ src/context/invoiceReducer.ts
@@ -0,0 +1,28 @@
+import type { Invoice } from "@/types";
+
+export type InvoiceAction =
+  | { type: "set"; payload: Invoice[] }
+  | { type: "add"; payload: Invoice }
+  | { type: "update"; payload: Invoice }
+  | { type: "delete"; payload: Invoice };
+
+export function invoicesReducer(invoices: Invoice[], { type, payload }: InvoiceAction): Invoice[] {
+  switch (type) {
+    case "set":
+      return payload;
+    case "add":
+      return [...invoices, payload];
+    case "delete":
+      return invoices.filter((invoice) => invoice.id !== payload.id);
+    case "update":
+      return invoices.map((invoice) => {
+        if (invoice.id == payload.id) {
+          return payload;
+        }
+
+        return invoice;
+      });
+    default:
+      return invoices;
+  }
+}
